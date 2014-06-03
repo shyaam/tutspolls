a33cd3d14dce0b9c4331cbad118480514d2a2fab
José Mota <jose@josemota.net>
2014-06-03 10:21:13 +0100

    Switch to Rails.

--- Guardfile
+++
@@ -1,2 +0,0 @@
-require "bootstrap-sass"
-guard 'sass', :input => 'app/assets/sass', :output => 'public/css'
--- app/base.rb
+++
@@ -1,9 +0,0 @@
-module Tutspolls
-  class App < Sinatra::Base
-    set :public_folder, File.expand_path("../../public", __FILE__)
-
-    get "/" do
-      haml :index
-    end
-  end
-end
